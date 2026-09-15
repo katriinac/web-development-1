d1c6d8206d1a924b4c1da85a887f1aa9f5715de5
katriinac <katriina.chindjapin@gmail.com>
2026-09-15 12:12:21 +0300

    harjoitus1

---
+++ Workshop3/js/script.js
@@ -0,0 +1,41 @@
+const button1 = document.getElementById("button1");
+const button2 = document.getElementById("button2");
+
+button1.addEventListener("click", function(){
+    alert("You clicked me!");
+});
+
+button2.addEventListener("click",function (){
+    showTable();
+});
+function showTable(){
+    const animal = "Tiikeri";
+    const habitat = "Metsä";
+    const diet = "Liha";
+    const animal2 = "Norsu";
+    const habitat2 = "Savanni";
+    const diet2 = "Kasvit";
+
+    const table = `
+        <table>
+            <tr>
+                <th>Eläin</th>
+                <th>Elinympäristö</th>
+                <th>Ruokavalio</th>
+            </tr>
+            <tr>
+                <td>${animal}</td>
+                <td>${habitat}</td>
+                <td>${diet}</td>
+            </tr>
+            <tr>
+                <td>${animal2}</td>
+                <td>${habitat2}</td>
+                <td>${diet2}</td>
+            </tr>
+        </table>
+    `;
+
+    const container = document.querySelector("#tableContainer");
+    container.innerHTML = table;
+};
