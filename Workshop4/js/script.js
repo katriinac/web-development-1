9071190414b77f71decc9bc3733bf14700946697
katriinac <katriina.chindjapin@gmail.com>
2026-09-24 13:59:23 +0300

    teht 1 ja 2 valmis versio

--- Workshop4/js/script.js
+++ Workshop4/js/script.js
@@ -1,3 +1,48 @@
+const changeHeading = document.querySelector("#changeHeadingButton");
+const taskOneHeading = document.querySelector("#taskOneHeading");
+changeHeading.addEventListener("click", function () {
+    taskOneHeading.textContent = "Muokattu otsikko!";
+});
+
+const changeStyle = document.querySelector("#changeStyleButton");
+changeStyle.addEventListener("click", function (){
+    taskOneHeading.classList.toggle("highlight");
+});
+
+const changeText = document.querySelector("#changeTextButton");
+const animalText = document.querySelector("#animalText");
+changeText.addEventListener("click", function (){
+    animalText.textContent = "Tiikerit ovat suuria petoeläimiä.";
+});
+
+const animalContent = document.querySelector("#animalContent");
+const animalH3 = document.createElement("h3");
+animalH3.textContent = "Päivän eläin.";
+
+const animalPara = document.createElement("p");
+animalPara.textContent = "Isot kissat ovat söpöjä";
+
+const animalImg = document.createElement("img");
+animalImg.src = "images/tiger.png";
+animalImg.alt = "Tiger";
+
+animalContent.append(animalH3);
+animalContent.append(animalPara);
+animalContent.append(animalImg);
+
+animalH3.classList.add("animal-heading");
+
+const hideAnimal =document.querySelector("#hideAnimalButton");
+const showAnimal = document.querySelector("#showAnimalButton");
+
+hideAnimal.addEventListener("click", function(){
+    animalContent.hidden = true;
+});
+
+showAnimal.addEventListener("click", function(){
+    animalContent.hidden = false;
+});
+
 // -------------------------------------------------- EXAMPLE 1 ANIMAL TABLE
 // -------------------------------------------------- EXAMPLE 1 ANIMAL TABLE
 
@@ -8,6 +53,7 @@ animalButton.addEventListener("click", function () {
     animalTable.hidden = !animalTable.hidden;
     console.log("Button Pressed");
 });
+
 
 // -------------------------------------------------- EXAMPLE 3 LISTEN DROPDOWN SELECT
 // -------------------------------------------------- EXAMPLE 3 LISTEN DROPDOWN SELECT
